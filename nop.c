63a05e52d5dc0f5daec6635c39106c5e7223f7b4
root <root@LAPTOP-I8GVERUB.localdomain>
2022-08-20 18:52:59 +0200

    nop opcode

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * nop - doesn’t do anything
+ * @stack: the pointer to the stack
+ * @line_number: line of the file that being trated
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+(void)stack;
+(void)line_number;
+
+}
