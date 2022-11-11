35a3dbc007bca6fe9143eff6284bd919ca521cf8
hedi <hedihamida98@gmail.com>
2022-11-11 15:43:22 +0100

    betty

--- interpret_line.c
+++ interpret_line.c
@@ -8,24 +8,24 @@
  */
 void interpret_line(char *line, unsigned int line_number, stack_m **stack)
 {
-    char *function_name;
-    int inoperative;
+	char *function_name;
+	int inoperative;
 
-    function_name = strtok(line, " \t\n");
+	function_name = strtok(line, " \t\n");
 
-    if (function_name != NULL)
-    {
-        input = strtok(NULL, " \t\n");
+	if (function_name != NULL)
+	{
+		input = strtok(NULL, " \t\n");
 
-        inoperative = valid(function_name);
-        if (inoperative != VALID)
-        {
-            print_error(line, line_number);
-            free_stack(stack);
-            free(line);
-            exit(EXIT_FAILURE);
-        }
-        else
-            getopfunc(function_name, line_number, stack);
-    }
+		inoperative = valid(function_name);
+		if (inoperative != VALID)
+		{
+			print_error(line, line_number);
+			free_stack(stack);
+			free(line);
+			exit(EXIT_FAILURE);
+		}
+		else
+			getopfunc(function_name, line_number, stack);
+	}
 }
