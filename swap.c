fee587fbd94ccfd24368afc41f6c912b367240aa
hedi <hedihamida98@gmail.com>
2022-11-11 23:17:28 +0100

    swap

--- swap.c
+++ swap.c
@@ -14,4 +14,17 @@ void swap(stack_m **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+	else
+	{
+		stack_m *node1 = (*stack);
+		stack_m *node2 = (*stack)->next;
+
+		node1->next = node2->next;
+		node1->prev = node2;
+
+		node2->prev = NULL;
+		node2->next = node1;
+
+		(*stack) = node2;
+	}
 }
